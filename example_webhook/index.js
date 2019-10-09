1016f1fa76a9fabae219d35067c32557f85de68c
remorses <beats.by.morse@gmail.com>
2019-10-09 21:42:19 +0200

    example_webhook/index: works

--- example_webhook/index.js
+++ example_webhook/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 80
 
+// parse application/json
+app.use(bodyParser.json())
 app.post('/', (req, res) => {
-    console.log('received email ' + JSON.parse(req.body).subject)
+    console.log('received email ' + req.body.subject)
     res.send('Hello World!')
 })
 
